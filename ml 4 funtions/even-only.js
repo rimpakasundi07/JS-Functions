1e9b276f66b5ac4ba0aaec91cf67f3901f0da642
Rimpa Kasundi <rimpakasundi@gmail.com>
2025-08-12 18:07:17 +0530

    added even-only-JS

--- ml 4 funtions/even-only.js
+++ ml 4 funtions/even-only.js
@@ -4,18 +4,36 @@
  * return the sum of even numbers
  */
 
-function evenNumbersonly(numbers) {
-  const evens = [];
+// function evenNumbersonly(numbers) {
+//   const evens = [];
+//   for (const number of numbers) {
+//     if (number % 2 === 0) {
+//       console.log(number);
+//       evens.push(number);
+//     }
+//   }
+//   return evens;
+// }
+
+// const numbers = [5, 8, 9, 91, 24, 6];
+
+// const evens = evenNumbersonly(numbers);
+// console.log("even numbers are", evens);
+
+// -------------------------------------->>>>>>>>>
+
+function sumOfEvenNumbers(numbers) {
+  let sum = 0; // Start with 0
   for (const number of numbers) {
     if (number % 2 === 0) {
-      console.log(number);
-      evens.push(number);
+      console.log(number); // Log the even number
+      sum += number; // Add it to sum
     }
   }
-  return evens;
+  return sum; // Return the total sum
 }
 
 const numbers = [5, 8, 9, 91, 24, 6];
+const sum = sumOfEvenNumbers(numbers);
 
-const evens = evenNumbersonly(numbers);
-console.log("even numbers are", evens);
+console.log("sum of the even numbers is", sum);
